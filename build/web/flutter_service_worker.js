cf9befadf75e642f57bf6a5ae9a0e1897ed457ad
saliousowce4 <saliousowce4@gmail.com>
2025-08-29 11:43:29 +0000

    Add proper sqflite web files for Floor database compatibility

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,15 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2eb090f6055e235be4d35670324067ad",
+const RESOURCES = {"flutter_bootstrap.js": "7f491c7338e72c55be9aae72b0e3203e",
 "version.json": "65333fd00e400229adc7a0e6e64b9724",
 "index.html": "d924b0ab33b485026e82555c27972025",
 "/": "d924b0ab33b485026e82555c27972025",
 "main.dart.js": "20a39ebbd5c222c5d94d546d5fd44d65",
-"sqlite3.wasm": "3be7b8b182ccd96e48989b4e57311193",
+"sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"sqflite_sw.js": "3be7b8b182ccd96e48989b4e57311193",
+"sqflite_sw.js": "2e34bbcfb90656c024e8428d43830339",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
